2d2004e2fdc0f3cfcd38be81eb51ef2d8fd8aafe
Sam Maosa <smaosa@strathmore.edu>
2023-07-19 23:13:43 +0300

    Bug Fix: Check for Filament login before checking permissions

--- src/Concerns/ContextualPage.php
+++ src/Concerns/ContextualPage.php
@@ -11,15 +11,21 @@ trait ContextualPage
     {
         $slug = static::getSlug();
 
-        return Filament::currentContext().".pages.{$slug}";
+        return Filament::currentContext() . ".pages.{$slug}";
     }
-    public static function getModuleName() {
+
+    public static function getModuleName()
+    {
         return \Str::of(Filament::currentContext())->before('-')->studly();
     }
-    public static function hasAccess() {
+
+    public static function hasAccess()
+    {
         return FilamentModules::hasAuthorizedAccess(Filament::currentContext());
     }
-    public static function bootContextualPage() {
-        abort_if(!static::hasAccess(),403,"You don't have access to the ".static::getModuleName()." module.");
+
+    public static function bootContextualPage()
+    {
+        abort_if(!static::hasAccess(), 403, "You don't have access to the " . static::getModuleName() . " module.");
     }
 }
