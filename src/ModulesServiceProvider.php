3e912fe8bc28f90431c85d086ac4a778f77d1d0f
Sam Maosa <smaosa@strathmore.edu>
2023-07-31 11:14:39 +0300

    Updated the sidebar render hooks according to Filament's latest update of the hooks

--- src/ModulesServiceProvider.php
+++ src/ModulesServiceProvider.php
@@ -39,13 +39,20 @@ class ModulesServiceProvider extends PackageServiceProvider
                     $title = $id->replace(['::', '-'], [' ', ' '])->title()->toString();
                     $panel
                         ->renderHook(
-                            'sidebar.start',
+                            'panels::sidebar.nav.start',
                             fn() => new HtmlString("<h2 class='m-2 p-2 font-black text-xl'>$title</h2>"),
-                        )->renderHook(
-                            'sidebar.end',
-                            fn() => new HtmlString("<a href='".url("/")."' class='m-2 p-2 block rounded-lg font-bold bg-primary-500/10 text-primary-500'>Go to Home</a>"),
                         )
-                    ;
+                        ->renderHook(
+                            'panels::sidebar.nav.end',
+                            fn() => new HtmlString(
+                                '<a href="' . url('/') . '" class="m-2 p-2 mt-4 inline-flex gap-2 block rounded-lg font-bold bg-gray-500/10">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                                        </svg>
+                                        Main Panel
+                                      </a>'
+                            ),
+                        );
                 }
             }
         });
