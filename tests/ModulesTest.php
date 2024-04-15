94d4c98cbd0c1657b83b091b32ca36825bc64752
Sam Maosa <smaosa@strathmore.edu>
2024-04-15 11:41:47 +0300

    Fixed Tests - Removed laravel 10 jobs from the workflow in favor of laravel 11

--- tests/ModulesTest.php
+++ tests/ModulesTest.php
@@ -3,10 +3,3 @@
 it('can test', function () {
     expect(true)->toBeTrue();
 });
-
-// test that a module can be generated successfully
-it('can generate a module', function () {
-    $this->artisan('module:make', ['name' => ['Example']])
-        ->expectsOutput('Module created successfully.')
-        ->assertExitCode(0);
-});
