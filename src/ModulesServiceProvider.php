431fee6c5f5f81fa527023c585b5858e94c6567f
Zuko <tansautn@gmail.com>
2026-08-14 15:09:21 +0700

    Remove file headers

--- src/ModulesServiceProvider.php
+++ src/ModulesServiceProvider.php
@@ -1,19 +1,4 @@
 <?php
-/*
- *          M""""""""`M            dP
- *          Mmmmmm   .M            88
- *          MMMMP  .MMM  dP    dP  88  .dP   .d8888b.
- *          MMP  .MMMMM  88    88  88888"    88'  `88
- *          M' .MMMMMMM  88.  .88  88  `8b.  88.  .88
- *          M         M  `88888P'  dP   `YP  `88888P'
- *          MMMMMMMMMMM    -*-  Created by Zuko  -*-
- *
- *          * * * * * * * * * * * * * * * * * * * * *
- *          * -    - -   F.R.E.E.M.I.N.D   - -    - *
- *          * -  Copyright © 2026 (Z) Programing  - *
- *          *    -  -  All Rights Reserved  -  -    *
- *          * * * * * * * * * * * * * * * * * * * * *
- */
 
 namespace Coolsam\Modules;
 
